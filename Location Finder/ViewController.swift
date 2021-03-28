341ac0b4bcaf900d1d62f21395c28bf040d8c30d
amar84 <72990540+amar84@users.noreply.github.com>
2021-03-28 14:53:12 -0400

    adding privacy and speech authorization and clearing errors

--- Location Finder/ViewController.swift
+++ Location Finder/ViewController.swift
@@ -8,13 +8,16 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Speech
 
 
 
 
-class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDelegate, MKMapViewDelegate {
+class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDelegate, MKMapViewDelegate, SFSpeechRecognizerDelegate  {
 
-   
+    //initializing speech components
+    @IBOutlet weak var btn_start: UIButton!
+    @IBOutlet weak var lb_speech: UILabel!
   
     @IBOutlet weak var address: UITextField!
     @IBOutlet weak var mapView: MKMapView!
@@ -60,7 +63,9 @@ class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDe
                        annotation.title = "YOU DESTINATION"
                        
                    }
+                    
     }
+        
         
         
         
@@ -72,6 +77,8 @@ class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDe
         reloadInputViews()
         
                 }
+    
+    
     
     
 
@@ -157,6 +164,106 @@ class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDe
         render.strokeColor = .green
         return render
     }
+    
+    // speech variables
+    let audioEngine = AVAudioEngine()
+    let speechReconizer : SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var task : SFSpeechRecognitionTask!
+    var isStart : Bool = false
+    
+    var globalJackpot = 0
+    
+    
+    
+    // defening the start button
+    
+    @IBAction func btn_start_stop(_ sender: Any) {
+           //MARK:- Coding for start and stop sppech recognization...!
+           isStart = !isStart
+           if isStart {
+               startSpeechRecognization()
+               btn_start.setTitle("STOP", for: .normal)
+               btn_start.backgroundColor = .systemGreen
+           }else {
+               cancelSpeechRecognization()
+               btn_start.setTitle("START", for: .normal)
+               btn_start.backgroundColor = .systemOrange
+           }
+       }
+    
+    // alert message
+    func alertView(message : String) {
+        let contrller = UIAlertController.init(title: "Error in accessing..!", message: message, preferredStyle: .alert)
+        contrller.addAction(UIAlertAction(title: "OK", style: .default , handler: { (_) in
+            contrller.dismiss(animated: true, completion: nil)
+        }))
+        self.present(contrller, animated: true, completion: nil)
+    }
+    
+   
+    
+    
+    // starting the speech recoginisation
+    func startSpeechRecognization(){
+            let node = audioEngine.inputNode
+            let recordingFormat = node.outputFormat(forBus: 0)
+            
+            node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, _) in
+                self.request.append(buffer)
+            }
+            
+            audioEngine.prepare()
+            do {
+                try audioEngine.start()
+            } catch let error {
+                alertView(message: "Error comes here for starting the audio listner =\(error.localizedDescription)")
+            }
+            
+            guard let myRecognization = SFSpeechRecognizer() else {
+                self.alertView(message: "Recognization is not allow on your local")
+                return
+            }
+            
+            if !myRecognization.isAvailable {
+                self.alertView(message: "Recognization is free right now, Please try again after some time.")
+            }
+            
+            task = speechReconizer?.recognitionTask(with: request, resultHandler: { (response, error) in
+                guard let response = response else {
+                    if error != nil {
+                        self.alertView(message: error.debugDescription)
+                    }else {
+                        self.alertView(message: "Problem in giving the response")
+                    }
+                    return
+                }
+                
+                let message = response.bestTranscription.formattedString
+                print("Message : \(message)")
+                self.lb_speech.text = message
+                
+                
+            })}
+        
+        // cancelling the speech
+        func cancelSpeechRecognization() {
+               task.finish()
+               task.cancel()
+               task = nil
+               
+               request.endAudio()
+               audioEngine.stop()
+              
+               if audioEngine.inputNode.numberOfInputs > 0 {
+                   audioEngine.inputNode.removeTap(onBus: 0)
+               }
+            
+    
+  
+    
+    }
+    
     
 }
 
